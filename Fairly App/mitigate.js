9e9921ed8b050fe8e1d0a55cea9640863b466b00
xrb2 <146208415+xrb2@users.noreply.github.com>
2025-03-02 09:01:50 -0500

    Delete Fairly App/mitigate.js

--- Fairly App/mitigate.js
+++
@@ -1,7 +0,0 @@
-const cache = {};
-async function fetchStorePrices(productName) {
-  if (cache[productName]) return cache[productName];
-  const prices = await fetchStorePricesImpl(productName); // Rename original function
-  cache[productName] = prices;
-  return prices;
-}
